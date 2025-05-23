355774c8e8665b3857c72742fb13098bc524ec0e
Larbi Adouhane <adouhanelaarbi@gmail.com>
2025-05-23 19:05:34 +0100

    Add files via upload

---
+++ Restaurant/app.js
@@ -0,0 +1,86 @@
+const menu = [
+    {
+        id: 1,
+        name: "Harira",
+        price: 18.00,
+        image: "H(1).jpg",
+        description: "soupe traditionnelle marocaine à base de tomates, lentilles, pois chiches et coriandre"
+    },
+    {
+        id: 2,
+        name: "Tajine de poulet",
+        price: 24.00,
+        image: "T(1).jpg",
+        description: "Poulet, olives, citrons confits, oignon, ail, épices (gingembre, curcuma, paprika, poivre noir), coriandre, huile d'olive"
+    },
+    {
+        id: 3,
+        name: "Thé à la menthe",
+        price: 12.00,
+        image: "THE (1).jpg",
+        description: "Eau, thé vert (de préférence thé Gunpowder), feuilles de menthe fraîche, sucre (facultatif)."
+    },
+    {
+        id: 4,
+        name: "Pastilla",
+        price: 60.00,
+        image: "P(1).jpg",
+        description: "poulet, oignon, ail, gingembre, curcuma, cannelle, poivre noir, coriandre, persil, amandes, sucre glace, beurre fondu, feuilles de brick."
+    },
+    {
+        id: 5,
+        name: "Briouates",
+        price: 8.00,
+        image: "B (1).jpg",
+        description: "petits chaussons feuilletés farcis de viande hachée, d'amandes et d'épices"
+    },
+    {
+        id: 6,
+        name: "CHEESE PIZZA",
+        price: 18.00,
+        image: "food6.png",
+        description: "Pâte à pizza, sauce tomate, fromage mozzarella râpé, optionnellement parmesan râpé, basilic frais, sel et poivre."
+    },
+    {
+        id: 7,
+        name: "Couscous royal",
+        price: 40.00,
+        image: "C (1).jpg",
+        description: "couscous servi avec un mélange de viandes : agneau, poulet et merguez, accompagné de légumes tels que carottes, courgettes et navets"
+    },
+    {
+        id: 8,
+        name: "Baklava",
+        price: 14.00,
+        image: "BAC (1).jpg",
+        description: "pâtisserie sucrée à base de feuilles de pâte filo, de noix ou d'amandes, et de sirop sucré parfumé à la fleur d'oranger"
+    },
+    {
+        id: 9,
+        name: "Tajine d'agneau",
+        price: 26.00,
+        image: "TAJIN (1).jpg",
+        description: "agneau mijoté avec des abricots secs, des amandes, des oignons, des épices et du miel, le tout cuit lentement dans un tajine"
+    },
+    {
+        id: 10,
+        name: "Rfissa",
+        price: 44.00,
+        image: "R (1).jpg",
+        description: "plat traditionnel à base de msemen ou de msemmen trempé dans une sauce au poulet, aux lentilles et aux oignons, parfois saupoudré de fenugrec moulu"
+    },
+    {
+        id: 11,
+        name: "Seffa medfouna",
+        price: 30.00,
+        image: "SE (1).jpg",
+        description: "vermicelles sucrées cuites à la vapeur et mélangées avec de la cannelle, du beurre et du sucre, servies avec des amandes grillées."
+    },
+    {
+        id: 12,
+        name: "Jus d'orange",
+        price: 11.00,
+        image: "JUS (1).jpg",
+        description: "Lorem ipsum dolor sit amet consectetu architecto expeditnostrum blanditiis, voluptatem reiciendis dolores laboriosam quod aperiam. Modi, quis."
+    }
+];
